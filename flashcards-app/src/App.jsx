0a8d6b6bddaa24dc6b680cf336ed3bb4eb1585f4
tokyo <tokyolabs@protonmail.com>
2024-02-09 10:32:05 -0500

    completed project

--- flashcards-app/src/App.jsx
+++ flashcards-app/src/App.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import questions from './data/questionsData';
 
 function App() {
+  const [selectedId, setSelectedId] = React.useState(null);
+
+  function handleCardClick(id) {
+    setSelectedId(id);
+  }
   return (
-    <>
-      <h1>Flashcards Coming Soon!</h1>
-    </>
+    <div className="flashcards">
+      {questions.map((question) => (
+        <div
+          key={question.id}
+          onClick={() => handleCardClick(question.id)}
+          className={question.id === selectedId ? 'selected' : ''}
+        >
+          <p>
+            {question.id === selectedId ? question.answer : question.question}
+          </p>
+        </div>
+      ))}
+    </div>
   );
 }
 
